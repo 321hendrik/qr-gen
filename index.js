16455fbe8fb3a704d8191bf8be1de92780da890d
Hendrik Elsner <hendrik.elsner@hpm-kommunikation.de>
2017-04-11 14:32:56 +0200

    generate from command line

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var qr = require('qr-image');
 var fs = require('fs');
 var path = require('path');
 
+if (process.argv[2]) {
+  var manuelGeneratedQrImagePath = path.join('output', 'qr_manual.png');
+  qr.image(process.argv[2], {
+    ec_level: 'Q',
+    size: 10,
+  }).pipe(fs.createWriteStream(manuelGeneratedQrImagePath));
+  require('child_process').spawn('open', [manuelGeneratedQrImagePath]);
+  return;
+}
+
 fs.readFile('urls.txt', 'utf8', function(err, contents) {
   contents.split('\n').forEach(function(elem,index) {
     qr.image(elem, {
